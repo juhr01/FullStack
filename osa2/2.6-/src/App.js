15789193819aff84c1244614e87eef3eab2c6fb0
juhr01 <juho1.riipinen@gmail.com>
2022-12-09 13:48:55 +0200

    2.11 aloitus

--- osa2/2.6-/src/App.js
+++ osa2/2.6-/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import AddPerson from './components/AddPerson'
 import FilterPersons from './components/FilterPersons'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -12,6 +13,18 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
