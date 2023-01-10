d9654abe82120047a00b413fd353fd33bcb450c2
juhr01 <juho1.riipinen@gmail.com>
2023-01-10 12:57:38 +0200

    3.8

--- osa3/3.1-/index.js
+++ osa3/3.1-/index.js
@@ -4,9 +4,10 @@ const nodemon = require('nodemon')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+app.use(morgan(':method :url :status - :response-time ms :body'))
 
 let persons = [
     {
