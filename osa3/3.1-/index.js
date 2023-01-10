3d0a14c3696507a1fe30ef3608c9a9790a4f5e9b
juhr01 <juho1.riipinen@gmail.com>
2023-01-10 10:25:52 +0200

    3.4

--- osa3/3.1-/index.js
+++ osa3/3.1-/index.js
@@ -60,12 +60,12 @@ app.get('/api/persons', (request, response) => {
     }
 })
 
-/* app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
-}) */
+})
 
 /* const generateId = () => {
   const maxId = notes.length > 0
