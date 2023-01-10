0216044f8907499e4fcd46275d101f02bced9621
juhr01 <juho1.riipinen@gmail.com>
2023-01-10 13:01:55 +0200

    3.8

--- osa3/3.1-/index.js
+++ osa3/3.1-/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
-app.use(morgan(':method :url :status - :response-time ms :body'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
