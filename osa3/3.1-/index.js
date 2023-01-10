28bb34095f5139a76ccb61dff2b91df7c5c24447
juhr01 <juho1.riipinen@gmail.com>
2023-01-10 09:14:50 +0200

    3.1

---
+++ osa3/3.1-/index.js
@@ -0,0 +1,96 @@
+const { response } = require('express')
+const express = require('express')
+const nodemon = require('nodemon')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    /* const note = notes.find(note => note.id === id)
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    }) */
+    console.log(note)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/api/persons', (request, response) => {
+    if (persons) {
+        response.json(persons)
+    } else {
+        response.status(404).end()
+    }
+})
+
+/* app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+}) */
+
+/* const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+} */
+
+/* app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+}) */
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
