e3a5060f28aefc8bd4f5c2a582e362c08ce15251
juhr01 <juho1.riipinen@gmail.com>
2023-02-14 12:58:33 +0200

    4.9

--- osa4/4.1-/tests/blog_api.test.js
+++ osa4/4.1-/tests/blog_api.test.js
@@ -4,6 +4,36 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -15,6 +45,11 @@ test('2 blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(2)
 } )
+
+test('identifying field is id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toBeDefined
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
