a35d8fe3f76c809eb3984fd01f108b9fe7260675
juhr01 <juho1.riipinen@gmail.com>
2023-02-20 13:12:58 +0200

    4.15, test not working

--- osa4/4.1-/models/blog.js
+++ osa4/4.1-/models/blog.js
@@ -4,7 +4,14 @@ const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 blogSchema.set('toJSON', {
