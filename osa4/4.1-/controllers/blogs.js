fc99068a9b0cedf2261250155b0c97be397eef0e
juhr01 <juho1.riipinen@gmail.com>
2023-02-21 12:16:06 +0200

    4.17

--- osa4/4.1-/controllers/blogs.js
+++ osa4/4.1-/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-   const blogs = await Blog.find({})
+   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
    response.json(blogs)
   })
 
@@ -18,14 +19,19 @@ blogsRouter.get('/', async (request, response) => {
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const user = await User.findById(body.user)
+
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes
+      likes: body.likes,
+      user: user._id
     })
   
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(blog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   })
 
