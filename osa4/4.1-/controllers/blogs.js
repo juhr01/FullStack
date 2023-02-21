5defb75cc59b0aca66e6436b05123e2c91c40b9d
juhr01 <juho1.riipinen@gmail.com>
2023-02-21 13:14:07 +0200

    4.18

--- osa4/4.1-/controllers/blogs.js
+++ osa4/4.1-/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -16,9 +17,20 @@ blogsRouter.get('/', async (request, response) => {
     }
   })
   
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer', '')
+  }
+  return null
+}
+
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
-
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({error: 'token invalid'})
+    }
     const user = await User.findById(body.user)
 
     const blog = new Blog({
