5969b549007ff410cad9e1dfbfa22dbaa1c11a95
juhr01 <juho1.riipinen@gmail.com>
2023-04-04 13:00:43 +0300

    5.17

--- osa4/4.1-/app.js
+++ osa4/4.1-/app.js
@@ -30,4 +30,9 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+  }
+
 module.exports = app
