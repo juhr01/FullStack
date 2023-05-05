247eba42b64797139f534f6a60a219b2b7a6feb4
juhr01 <juho1.riipinen@gmail.com>
2023-05-05 13:31:55 +0300

    6.21

--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -1,4 +1,11 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3001/anecdotes'
+
 export const getAnecdotes = () =>
-    axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+    axios.get(baseUrl).then(res => res.data)
+
+export const createAnecdote = newAnecdote => 
+    axios.post(baseUrl, newAnecdote).then(res => res.data)
+
+    
