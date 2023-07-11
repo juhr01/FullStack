d42625c91e3fbf055b26e210f10b6962614f2061
juhr01 <juho1.riipinen@gmail.com>
2023-07-11 08:46:51 +0300

    7.7

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -1,34 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const useField = (type) => {
+export const useField = (type) => {
   const [value, setValue] = useState('')
 
   const onChange = (event) => {
-    setValue(event.target.value)
+      setValue(event.target.value)
   }
 
   return {
-    type,
-    value,
-    onChange
+      type,
+      value,
+      onChange
   }
 }
 
-const useCountry = (name) => {
+export const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+      if (name) {
+          axios
+              .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}?fullText=true`)
+              .then((response) => {
+                  setCountry(response.data);
+              })
+              .catch((error) => {
+                  setCountry(null);
+              });
+      }
+  }, [name]);
 
-  return country
+  return country;
 }
 
 const Country = ({ country }) => {
   if (!country) {
-    return null
-  }
-
-  if (!country.found) {
     return (
       <div>
         not found...
@@ -36,12 +43,13 @@ const Country = ({ country }) => {
     )
   }
 
+
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>  
     </div>
   )
 }
