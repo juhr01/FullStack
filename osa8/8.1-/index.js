44d875c6545e946f1fd0689543a84c3010f158c2
juhr01 <juho1.riipinen@gmail.com>
2023-08-23 13:24:36 +0300

    8.2

--- osa8/8.1-/index.js
+++ osa8/8.1-/index.js
@@ -98,16 +98,33 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
+  },
+  Book: {
+    title: (root) => root.title,
+    published: (root) => root.published,
+    author: (root) => root.author,
+    id: (root) => root.id,
+    genres: (root) => root.genres
   }
 }
 
