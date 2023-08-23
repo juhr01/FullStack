c5e63cd1b6c2cd8bdcdda12a2dfcb1c7d9cc9790
juhr01 <juho1.riipinen@gmail.com>
2023-08-23 13:49:03 +0300

    8.4

--- osa8/8.1-/index.js
+++ osa8/8.1-/index.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -123,7 +123,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors,
   },
   Book: {
