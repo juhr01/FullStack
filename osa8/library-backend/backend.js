ab9c4ea3938ddb4854ab3b34e8b5a911e360bb6d
juhr01 <juho1.riipinen@gmail.com>
2023-10-04 13:03:31 +0300

    mongodb käyttöön

--- osa8/library-backend/backend.js
+++ osa8/library-backend/backend.js
@@ -1,102 +1,99 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v4: uuidv4 } = require('uuid')
+const config = require('./config')
+const mongoose = require('mongoose')
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-let authors = [
+mongoose.set('strictQuery', false)
+
+async function startServer() {
+  try {
+    await mongoose.connect(config.MONGODB_URI);
+    console.info('Connected to MongoDB');
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: config.PORT },
+    });
+
+    console.log(`Server ready at ${url}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+}
+
+
+/* let authors = [
   {
     name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
     born: 1952,
   },
   {
     name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
     born: 1963
   },
   {
     name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
     born: 1821
   },
   { 
     name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
   },
   { 
     name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
 
 let books = [
   {
     title: 'Clean Code',
     published: 2008,
     author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Agile software development',
     published: 2002,
     author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
     genres: ['agile', 'patterns', 'design']
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
     author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
     author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'patterns']
   },  
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
     author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'design']
   },
   {
     title: 'Crime and punishment',
     published: 1866,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'crime']
   },
   {
     title: 'The Demon ',
     published: 1872,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'revolution']
   },
-]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
+] */
 
 const typeDefs = `
   type Author {
@@ -108,7 +105,7 @@ const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     genres: [String!]!
     id: ID!
   }
@@ -228,13 +225,36 @@ const resolvers = {
   }
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+startServer();
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+/* async function populateCollections() {
+  try {
+    // Clear existing data (optional)
+    await Author.deleteMany({});
+    await Book.deleteMany({});
+
+    // Insert authors into the 'authors' collection
+    await Author.insertMany(authors);
+
+    // Update 'author' references in books
+    const authorMap = {}; // Map author names to their corresponding MongoDB _id
+    authors.forEach((author) => {
+      authorMap[author.name] = author._id;
+    });
+    books.forEach((book) => {
+      book.author = authorMap[book.author];
+    });
+
+    // Insert books into the 'books' collection
+    await Book.insertMany(books);
+
+    console.log('Data has been inserted into MongoDB collections.');
+  } catch (error) {
+    console.error('Error populating collections:', error.message);
+  } finally {
+    mongoose.disconnect(); // Close the MongoDB connection
+  }
+}
+
+// Call the function to populate the collections
+populateCollections(); */
