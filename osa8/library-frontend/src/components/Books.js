13e47bf7a22fad9788344a9c5c39b7a043090937
juhr01 <juho1.riipinen@gmail.com>
2023-10-12 12:56:21 +0300

    8.19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,21 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
 
 const Books = () => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [genres, setGenres] = useState([])
 
-  const result = useQuery(ALL_BOOKS, {
+  const { loading, data } = useQuery(ALL_BOOKS, {
     pollInterval: 2000
-  })
+  });
 
-  if (result.loading) {
+  useEffect(() => {
+    if (!loading && data) {
+      // Extract and set the available genres
+      const genres = [...new Set(data.allBooks.map((book) => book.genres).flat())];
+      setGenres(genres);
+    }
+  }, [loading, data]);
+
+
+  if (loading) {
     return <div>loading...</div>
   }
-  console.log(result)
 
   return (
     <div>
       <h2>books</h2>
-
+      <h3>filter by genre</h3>
+      <select onChange={e => setSelectedGenre(e.target.value)}>
+        <option value="">all genres</option>
+        {genres.map(g => (
+          <option key={g} value={g}>
+            {g}
+          </option>
+        ))}
+      </select>
       <table>
         <tbody>
           <tr>
@@ -23,7 +42,9 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map(b => (
+          {data.allBooks
+          .filter((book) => !selectedGenre || book.genres.includes(selectedGenre))
+          .map(b => (
             <tr key={b.id}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
